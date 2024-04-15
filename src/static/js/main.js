93fa6ac55169a680f4419f47419bcc49a3f7dc29
Vladislav Yaremenko <vlad.yaremenko.98@yandex.ru>
2024-04-15 10:50:43 +0300

    Form add order.

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,8 +1,10 @@
 const old_cities = $("#cities").val();
 const old_users = $("#users").val();
+const max_files = $("#id_images").attr("max-files");
+const old_src_user_image = $("#id_user_image").attr("src");
 let params = new URLSearchParams(window.location.search);
-let old_choice_min = Number($("#min-price").text()); 
-let old_choice_max = Number($("#max-price").text());
+let old_choice_min = Number($("#price").attr("min-price")); 
+let old_choice_max = Number($("#price").attr("max-price"));
 if (params.has("min_price"))
   old_choice_min = params.get("min_price");
 if (params.has("max_price"))
@@ -15,8 +17,8 @@ function filterOrders() {
   let price = $("#price").val();
   let regexp = new RegExp("\\d+", "g");
   price = Array.from(price.matchAll(regexp));
-  let min_price = price[0] ? price[0] : Number($( "#min-price" ).text());
-  let max_price = price[1] ? price[1] : Number($( "#max-price" ).text());
+  let min_price = price[0] ? price[0] : Number($("#price").attr("min-price")); 
+  let max_price = price[1] ? price[1] : Number($("#price").attr("max-price"));
   let cities = $("#cities").val();
   let users = $("#users").val();
   let uri = "/" + "?min_price=" + min_price + "&max_price=" + max_price;
@@ -41,12 +43,75 @@ function activateButtonFilter(
     $("#button-filter").attr("disabled", true);
 }
 
+function showModal() {
+  let form_order = $("#formOrder");
+  if (form_order.attr("errors") === "true")
+    form_order.modal('show');
+}
 
-$( function() {
+function validationPrice() {
+  let valid_var = $(this).val();
+  max_price
+  valid_var = valid_var.replace(/[^0-9]/g, "").replace(/^0+/, "");
+  $(this).val(valid_var);
+}
+
+function validationImages() {
+  let images = $("#id_images");
+  if (images[0].files.length > max_files) {
+    images.val(null);
+    images.attr("class", "form-control is-invalid")
+    $("#images_errors").html("<ul><li>Изображений должно быть до 4.</li></ul>");
+    return
+  }
+  images.attr("class", "form-control");
+  $("#images_errors").html("");
+}
+
+function setOrderImage() {
+  images = $("#id_images")[0].files;
+  let length = images.length;
+  let carousel = '<img class="d-block w-100 img border border-1 rounded" src="/static/images/no_data.jpg" alt="нет данных">';
+  if (length) {
+  let carousel_indicators = '<div class="carousel-indicators">';
+  let carousel_inner = '<div class="carousel-inner">';
+  for (i = 0; i < length; i++) {
+    let src = URL.createObjectURL(images[i]);
+    if (i === 0) {
+      carousel_indicators += '<button type="button" data-bs-target="#orderImageIndicators" ' +
+                              'data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>';
+      carousel_inner += '<div class="carousel-item active"><img src="' + src + '" class="d-block w-100 img border border-1 rounded" alt="Изображение ' + (i + 1) + '"></div>'
+    } else {
+      carousel_indicators += '<button type="button" data-bs-target="#orderImageIndicators" data-bs-slide-to="' + i + '" aria-label="Slide ' + (i + 1) + '"></button>';
+      carousel_inner += '<div class="carousel-item"><img src="' + src + '" class="d-block w-100 img border border-1 rounded" alt="Изображение ' + (i + 1) + '"></div>';
+    }
+  }
+  carousel_indicators += '</div>';
+  carousel_inner += '</div>';
+  carousel = carousel_indicators + carousel_inner + 
+              '<button class="carousel-control-prev" type="button" data-bs-target="#orderImageIndicators" data-bs-slide="prev">' +
+              '<span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span>' +
+              '</button><button class="carousel-control-next" type="button" data-bs-target="#orderImageIndicators" data-bs-slide="next">' +
+              '<span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></button>';
+  }
+  $("#orderImageIndicators").html(carousel);
+}
+
+
+function setUserImage() {
+  image = $("#id_user_picture")[0].files[0];
+  if(image)
+    $("#id_user_image").attr("src", URL.createObjectURL(image));
+  else
+    $("#id_user_image").attr("src", old_src_user_image);
+}
+
+
+$(function() {
     $("#slider-range").slider({
       range: true,
-      min: Number($("#min-price").text()),
-      max: Number($("#max-price").text()),
+      min: Number($("#price").attr("min-price")),
+      max: Number($("#price").attr("max-price")),
       values: [old_choice_min, old_choice_max],
       slide: function(event, ui) {
         new_choice_min = ui.values[0];
@@ -59,30 +124,17 @@ $( function() {
     });
     $("#price").val($("#slider-range").slider("values", 0) + "р." +
       " - " + $("#slider-range").slider("values", 1) + "р.");
-  } );
+  });
 $("#button-filter").on("click", filterOrders);
-$("#cities").change(function(){
+$("#cities").change(function() {
   activateButtonFilter(old_cities, old_users)
 });
-$("#users").change(function(){
+$("#users").change(function() {
   activateButtonFilter(old_cities, old_users)
 });
-$(".btn-toggle").click(function() {
-  $(this).find(".btn").toggleClass("active");  
-  
-  if ($(this).find(".btn-primary").length>0) {
-    $(this).find(".btn").toggleClass("btn-primary");
-  }
-  if ($(this).find(".btn-danger").length>0) {
-    $(this).find(".btn").toggleClass("btn-danger");
-  }
-  if ($(this).find(".btn-success").length>0) {
-    $(this).find(".btn").toggleClass("btn-success");
-  }
-  if ($(this).find(".btn-info").length>0) {
-    $(this).find(".btn").toggleClass("btn-info");
-  }
-  
-  $(this).find(".btn").toggleClass("btn-default");
-     
-});
+$(window).on("load", showModal);
+$("#id_price").on("input", validationPrice);
+$("#id_images").on("change", validationImages);
+$("#id_images").on("change", setOrderImage);
+$("#id_images").on("change", setOrderImage);
+$("#id_user_picture").on("change", setUserImage);
